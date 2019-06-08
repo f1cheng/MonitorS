279b0b878805db995c1ccd038bf496c613a308f1
Cheng Fei <cheng.fei1026@gmail.com>
2019-06-08 22:40:17 +0800

    Create db.py

---
+++ db.py
@@ -0,0 +1,15 @@
+## db
+```
+
+1. oracle cx_Oracle
+   db = cx_Oracle.connect("username/password@host:port/database")
+   self.db = cx_Oracle.connect("{0}/{1}@{2}:{3}/{4}" \
+             .format(username, pwd, address, port, database))
+2. mysql
+# db connect
+MYSQL_CONN="/usr/bin/mysqladmin -u${MYSQL_USER} -p${MYSQL_PWD} -h${MYSQL_HOST} -P${MYSQL_PORT}"
+result=`${MYSQL_CONN} status|cut -f2 -d":"|cut -f1 -d"T"`
+echo $result
+
+https://www.cnblogs.com/shenjianyu/p/6627843.html
+```
